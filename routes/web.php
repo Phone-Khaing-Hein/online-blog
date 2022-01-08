6b56e56e075cd912a61ce2f755aa753b102126a4
Rimuru-Tempest-26 <yellowdiamond228@gmail.com>
2022-01-08 20:05:51 +0630

    finish post

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::middleware('auth')->group(function(){
     Route::resource('category',\App\Http\Controllers\CategoryController::class);
+    Route::resource('post',\App\Http\Controllers\PostController::class);
 });
